a80aaf21e962a5a5af2b2b04dfdfce44f3152691
salim-ingram <98127818+salim-ingram@users.noreply.github.com>
2023-08-10 01:47:03 -0700

    Removed vpc config

--- terraform/aws/infra/outputs.tf
+++ terraform/aws/infra/outputs.tf
@@ -1,31 +1,3 @@
-output "vpc_id" {
-  value = aws_vpc.case-search-beta-vpc.id
-}
-
-output "ecs_security_group_id" {
-  value = aws_security_group.ecs.id
-}
-
-output "load_balancer_security_group_id" {
-  value = aws_security_group.load-balancer.id
-}
-
-output "public_subnet_1_id" {
-  value = aws_subnet.public-subnet-1.id
-}
-
-output "public_subnet_2_id" {
-  value = aws_subnet.public-subnet-2.id
-}
-
-output "private_subnet_1_id" {
-  value = aws_subnet.private-subnet-1.id
-}
-
-output "private_subnet_2_id" {
-  value = aws_subnet.private-subnet-2.id
-}
-
 output "lambda_bucket_name" {
   description = "Name of the S3 bucket used to store function code"
 
