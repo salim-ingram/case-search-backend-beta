bba56acca627ae3f5d7acb42620e781511e6078f
salim-ingram <98127818+salim-ingram@users.noreply.github.com>
2023-08-10 00:27:43 -0700

    Removed bucket acl

--- terraform/aws/infra/main.tf
+++ terraform/aws/infra/main.tf
@@ -14,8 +14,3 @@ resource "random_pet" "lambda_bucket_name" {
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = random_pet.lambda_bucket_name.id
 }
-
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  acl    = "private"
-}
